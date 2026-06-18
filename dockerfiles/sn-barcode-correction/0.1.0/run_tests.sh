93d5662ee81c5a27e89c4c38d697824cb0304f8d
David Urbina <durbina@ebi.ac.uk>
2026-06-18 09:58:32 +0100

    Remove image definitions and ci config for old versions of sn-barcode-correction, sn-barcode-preprocessing, and sn-fastq-cleanup-and-update

--- dockerfiles/sn-barcode-correction/0.1.0/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -uef
-set -o pipefail
-set -o xtrace
-python barcodes_correction.py --help
--- dockerfiles/sn-fastq-cleanup-and-update/0.5.0/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -uef
-set -o pipefail
-set -o xtrace
-
-python sn_fastq_filtering_update.py --help
