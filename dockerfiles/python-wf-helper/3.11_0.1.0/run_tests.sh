29f6a3d8ad095a4356fd07764d970e6f4434dcbe
David Urbina <durbina@ebi.ac.uk>
2026-09-16 12:16:07 +0100

    Update workflow helper dependencies

--- dockerfiles/python-wf-helper/3.11_0.1.0/run_tests.sh
+++ dockerfiles/python-wf-helper/3.11_0.1.0/run_tests.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
 set -uef
-set -o pipefail
 set -o xtrace
 
-python --version
+python - <<'PY'
+import boto3
+import numpy
+import pandas
+import pydantic
+import requests
+import urllib3
+
+assert pydantic.__version__.split(".", 1)[0] == "1"
+print(
+    f"boto3={boto3.__version__} numpy={numpy.__version__} "
+    f"pandas={pandas.__version__} pydantic={pydantic.__version__} "
+    f"requests={requests.__version__} urllib3={urllib3.__version__}"
+)
+PY
+
+python -m pip check
