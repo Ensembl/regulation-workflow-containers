c490ee577d70e1e80b587d12492824ecdca068e6
David Urbina <durbina@ebi.ac.uk>
2026-09-16 12:49:30 +0100

    Update vulnerable image dependencies

--- dockerfiles/sn-barcode-correction/0.2.0/run_tests.sh
+++ dockerfiles/sn-barcode-correction/0.2.0/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -uef
-set -o pipefail
 set -o xtrace
+
 python barcodes_correction.py --help
--- dockerfiles/sn-barcode-preprocessing/0.1.1/run_tests.sh
+++ dockerfiles/sn-barcode-preprocessing/0.1.1/run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -uef
-set -o pipefail
 set -o xtrace
 
 python sn_atac_barcodes_preprocessing.py --help
